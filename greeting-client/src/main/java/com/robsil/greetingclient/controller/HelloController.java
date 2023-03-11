9a90653d90396644acdf91fc6850e475da6b57af
RobSil <djtim2014@gmail.com>
2023-03-11 21:34:45 +0200

    make it in microservices............

--- greeting-client/src/main/java/com/robsil/greetingclient/controller/HelloController.java
+++ greeting-client/src/main/java/com/robsil/greetingclient/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.robsil.greetingclient.controller;
 import com.robsil.greetingclient.service.HelloService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/greetings")
 public class HelloController {
 
     private final HelloService helloService;
